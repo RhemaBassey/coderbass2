2e83253ea0b5b5644ccbcc6656bd079f64c45d25
unknown <bassey.rhema@yahoo.com>
2022-11-11 16:48:50 +0100

    made an MVP web scraper web app that allows 2 inputs, and retains input data after submitting

--- index.js
+++ index.js
@@ -26,24 +26,39 @@ app.get("/projects/web_scraper", (req, res) => {
   res.render(__dirname + "/projects/web scraper/views/index.ejs", {
     title: "",
     value: "",
+    tag1: "",
+    tag2: "",
+    url: ""
   });
 });
+
+
 app.post("/projects/web_scraper/send-url", urlEncodedParser, (req, res) => {
   let url = req.body.url;
+  let tag1 = req.body.tag1;
+  let tag2 = req.body.tag2;
+
+
+  
   // res.send("SENT IT!");
 
   (async () => {
+
     const response = await got(url);
 
     const $ = cheerio.load(response.body);
 
-    let title = $("h1").html();
+    let title = $(tag1).html();
     // let value = $('.ratingValue').find('[itemprop="ratingValue"]').html();
-    let value = $('span[class="sc-7ab21ed2-1 jGRxWM"]').html();
+    let value = $(tag2).html();
 
     res.render(__dirname + "/projects/web scraper/views/index.ejs", {
       title: title,
-      value: value});
+      value: value,
+      tag1: tag1,
+      tag2: tag2,
+      url:url
+    });
 
   })();
 });
